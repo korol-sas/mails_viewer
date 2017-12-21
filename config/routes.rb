0bc2e5ed4dacd9154161ee955a63e0ce4d29739c
Sudarshan Dhokale <sudarshand@kumolus.com>
2017-12-21 21:21:55 +0530

    Update routes.rb
    fix for the issue- Missing :action key on routes definition, please check your routes (rails 5)

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 MailsViewer::Engine.routes.draw do
   controller "home" do
-    get "/index", to: :index
-    get "/raw", to: :raw
-    get "/html", to: :html
-    get "/plain", to: :plain
-    get '/attachment', to: :attachment
+    get "/index" => 'home#index', to: :index
+    get "/raw" => 'home#raw', to: :raw
+    get "/html" => 'home#html', to: :html
+    get "/plain" => 'home#plain', to: :plain
+    get '/attachment' => 'home#attachment', to: :attachment
   end
   root to: 'home#index'
 end
